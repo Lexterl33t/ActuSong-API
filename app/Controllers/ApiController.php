e24d9214629a80c4587f7170b727c422c1abdb44
brytonl33t <paraghostsociety@gmail.com>
2022-01-10 13:56:18 +0100

    fix bug 1 day is defautl days feature

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controllers;
 
-use DI\Container;
-use GuzzleHttp\Client;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -24,59 +22,56 @@ class ApiController
 
     public function get_access_token($force = false)
     {
-        if(!file_exists(dirname(__FILE__).'/../../token_access.json'))
+        if (!file_exists(dirname(__FILE__) . '/../../token_access.json'))
             return false;
 
-        if(!$force) {
-            $get_content = json_decode(file_get_contents(dirname(__FILE__).'/../../token_access.json'));
+        if (!$force) {
+            $get_content = json_decode(file_get_contents(dirname(__FILE__) . '/../../token_access.json'));
 
-            if(!empty($get_content->access_token)) {
+            if (!empty($get_content->access_token)) {
                 return $get_content->access_token;
             }
         }
 
 
         $server_output = RequestController::post("https://accounts.spotify.com/api/token", [
-            'Authorization:  Basic '.base64_encode($this->app_settings['client_id'].':'.$this->app_settings['client_secret'])
+            'Authorization:  Basic ' . base64_encode($this->app_settings['client_id'] . ':' . $this->app_settings['client_secret'])
         ], "grant_type=client_credentials");
 
 
-
-        file_put_contents(dirname(__FILE__).'/../../token_access.json', "{\"access_token\": \"".json_decode($server_output)->access_token."\"}");
+        file_put_contents(dirname(__FILE__) . '/../../token_access.json', "{\"access_token\": \"" . json_decode($server_output)->access_token . "\"}");
         return json_decode($server_output)->access_token;
     }
 
     public function actuality_album(RequestInterface $request, ResponseInterface $response, $args)
     {
-        $get_all_artists_in_array = json_decode(json_encode(json_decode(file_get_contents(dirname(__FILE__).'/../../artists.json'))), true);
+        $get_all_artists_in_array = json_decode(json_encode(json_decode(file_get_contents(dirname(__FILE__) . '/../../artists.json'))), true);
 
         $access_token = $this->get_access_token();
 
         $recent_album = [];
 
-        if((!isset($args['date_interval_start']) && empty($args['date_interval_start'])) && (!isset($args['date_interval_end']) && empty($args['date_interval_end'])) && (!isset($args['default_days']) && empty($args['default_days']))) {
+        if ((!isset($args['date_interval_start']) && empty($args['date_interval_start'])) && (!isset($args['date_interval_end']) && empty($args['date_interval_end'])) && (!isset($args['default_days']) && empty($args['default_days']))) {
             $date_start = strtotime("now");
             $date_end = strtotime("-30 days");
             $cacheName = "recent_artists_album";
-        }  elseif (isset($args['default_days']) && !empty($args['default_days'])){
+        } elseif (isset($args['default_days']) && !empty($args['default_days'])) {
             $date_start = strtotime("now");
-            if($args['default_days'] === "now" || $args['default_days'] === "0")
-                $date_end = strtotime("now");
-            else
-                $date_end = strtotime("-".$args['default_days']." days");
 
-            $cacheName = "recent_artists_album_".$args['default_days'];
+            $date_end = strtotime("-" . $args['default_days'] . " days");
+
+            $cacheName = "recent_artists_album_" . $args['default_days'];
         } else {
             $date_start = strtotime($args['date_interval_start']);
             $date_end = strtotime($args['date_interval_end']);
-            $cacheName = "recent_artist_albums_".$date_end.'_'.$date_start;
+            $cacheName = "recent_artist_albums_" . $date_end . '_' . $date_start;
         }
 
-        if(!$this->container->get('cache')->read($cacheName)) {
-            foreach($get_all_artists_in_array['artists'] as $artist) {
+        if (!$this->container->get('cache')->read($cacheName)) {
+            foreach ($get_all_artists_in_array['artists'] as $artist) {
 
-                $req = RequestController::get('https://api.spotify.com/v1/artists/'.$artist['id'].'/albums', [
-                    'Authorization: Bearer '.$access_token,
+                $req = RequestController::get('https://api.spotify.com/v1/artists/' . $artist['id'] . '/albums', [
+                    'Authorization: Bearer ' . $access_token,
                     'Accept: application/json',
                     'Content-Type: application/json'
                 ], [
@@ -84,14 +79,14 @@ class ApiController
                 ]);
 
                 $req_array = json_decode($req, true);
-                if(isset($req_array['error']) && $req_array['error']['message'] === "The access token expired") {
+                if (isset($req_array['error']) && $req_array['error']['message'] === "The access token expired") {
                     $this->get_access_token(true);
                     $response->getBody()->write("Refresh page");
                     return $response;
                 }
                 $released_date = strtotime($req_array['items'][0]['release_date']);
-                if($date_start < $date_end) {
-                    if($released_date >= $date_start && $released_date <= $date_end) {
+                if ($date_start < $date_end) {
+                    if ($released_date >= $date_start && $released_date <= $date_end) {
                         $data = [
                             'name' => $req_array['items'][0]['artists'][0]['name'],
                             'recent_album' => $req_array['items'][0]['name'],
@@ -100,7 +95,7 @@ class ApiController
                         array_push($recent_album, $data);
                     }
                 } else {
-                    if($released_date <= $date_start && $released_date >= $date_end) {
+                    if ($released_date <= $date_start && $released_date >= $date_end) {
                         $data = [
                             'name' => $req_array['items'][0]['artists'][0]['name'],
                             'recent_album' => $req_array['items'][0]['name'],
